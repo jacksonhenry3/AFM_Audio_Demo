2aef7742f0409b45c134d6f4bb738b09eab2bb53
Jackson Henry <jacksonhenry3@gmail.com>
2019-07-10 12:24:00 -0400

    reduced frequency

--- js/audio.js
+++ js/audio.js
@@ -25,7 +25,7 @@ function playNote(freq, type){
   gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
 
   oscillator.start();
-  gainNode.gain.linearRampToValueAtTime(.2, audioCtx.currentTime + .1);
+  gainNode.gain.linearRampToValueAtTime(.1, audioCtx.currentTime + .1);
   return [oscillator,gainNode]
 }
 
@@ -42,20 +42,20 @@ freqObj = {
 }
 
 freqObj = {
-  "1":261.63,
-  "2":293.66,
-  "3":329.63,
-  "4":349.23,
-  "5":392.00,
-  "6":440.00,
-  "7":493.88,
-  "8":523.25,
-  "9":587.33,
-  "0":659.25,
-  "½":783.99
+  "1":261.63/2,
+  "2":293.66/2,
+  "3":329.63/2,
+  "4":349.23/2,
+  "5":392.00/2,
+  "6":440.00/2,
+  "7":493.88/2,
+  "8":523.25/2
+  // "9":587.33/4,
+  // "0":659.25/4,
+  // "½":783.99/4
 }
 
-
+var n =1
 $("#myRange").on('input', function(){console.log("A")});
 
 var slider = document.getElementById("myRange");
@@ -63,25 +63,54 @@ var slider = document.getElementById("myRange");
 // output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.onmouseout = function(){ if (micQQ) {source.connect(analyserNode);
-source.connect(audioCtx.destination);
+slider.onmouseout = function(){ micQ = false;sliderGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + .1);console.log(  sliderOscilator.frequency.value);
 
-}micQ = false;sliderGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + .1);};
+  if (typeof micQQ !== 'undefined') {
+    if (micQQ) {source.connect(analyserNode);
+  }
+
+
+if (typeof source !== 'undefined') {
+  source.connect(audioCtx.destination);
+}
+}};
+
+
+slider.ontouchend = function(){ micQ = false;sliderGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + .1);console.log(  sliderOscilator.frequency.value);
+
+  if (typeof micQQ !== 'undefined') {
+    if (micQQ) {source.connect(analyserNode);
+  }
+
+
+if (typeof source !== 'undefined') {
+  source.connect(audioCtx.destination);
+}
+}};
+
+
+
 slider.oninput = function() {
   micQ = true
-  source.disconnect()
-  sliderOscilator.frequency.setValueAtTime(this.value*15+100, audioCtx.currentTime);
-  sliderGainNode.gain.linearRampToValueAtTime(.2, audioCtx.currentTime + .1);
+
+  sliderOscilator.frequency.setValueAtTime(Math.exp(this.value), audioCtx.currentTime);
+  sliderGainNode.gain.linearRampToValueAtTime(.1, audioCtx.currentTime + .1);
+
+  if (typeof source !== 'undefined') {
+    source.disconnect()
+}
+
 }
 
 
 
 
 $(document).keydown(function(event){
-  if (oscilatorObj[event.keyCode] == null && ! micQ ) {
-      note = String.fromCharCode(event.keyCode).toLowerCase()
+  note = String.fromCharCode(event.keyCode).toLowerCase()
+  if (oscilatorObj[note] == null && ! micQ ) {
+
       console.log(note)
-      oscilatorObj[event.keyCode] = playNote(freqObj[note],oscType)
+      oscilatorObj[note] = playNote(freqObj[note],oscType)
   }
 
 
@@ -90,10 +119,11 @@ $(document).keydown(function(event){
 });
 $(document).keyup(function(event){
     if (! micQ) {
+      note = String.fromCharCode(event.keyCode).toLowerCase()
 
-    oscilatorObj[event.keyCode][1].gain.linearRampToValueAtTime(0, audioCtx.currentTime + .1);
-    oscilatorObj[event.keyCode][0].stop(audioCtx.currentTime + .15);
-    oscilatorObj[event.keyCode] = null
+    oscilatorObj[note][1].gain.linearRampToValueAtTime(0, audioCtx.currentTime + .1);
+    oscilatorObj[note][0].stop(audioCtx.currentTime + .15);
+    oscilatorObj[note] = null
   }
 
 
@@ -106,6 +136,37 @@ var lastClickedID = "sine"
 var micQ = false
 $("#"+lastClickedID).css("box-shadow", "0px 0px 0px #444");
 $("#"+lastClickedID).css("opacity", "1");
+
+
+
+
+
+$(".key").bind("touchstart mousedown",function(){
+  console.log(this.innerHTML)
+  if (oscilatorObj[this.innerHTML] == null && ! micQ ) {
+      note = parseFloat(this.innerHTML)
+      console.log(typeof note)
+      oscilatorObj[this.innerHTML] = playNote(freqObj[note],oscType)
+  }
+
+});
+
+
+$(".key").bind("touchend mouseup",function(){
+    if (! micQ) {
+      note = parseFloat(this.innerHTML)
+
+    oscilatorObj[this.innerHTML][1].gain.linearRampToValueAtTime(0, audioCtx.currentTime + .1);
+    oscilatorObj[this.innerHTML][0].stop(audioCtx.currentTime + .15);
+    oscilatorObj[this.innerHTML] = null
+  }
+
+
+var micQQ = false;
+
+});
+
+
 
 
 $(".button").click(function(){
@@ -173,43 +234,122 @@ var stream;
 
 
 analyserNode.fftSize = 2048;
-analyserNode.fftSize = 4096;
 var bufferLength = analyserNode.frequencyBinCount;
 var dataArray = new Uint8Array(bufferLength);
 analyserNode.getByteTimeDomainData(dataArray);
 
 // Get a canvas defined with ID "oscilloscope"
 var canvas = document.getElementById("oscilloscope");
+canvas.width = 1315;
+canvas.height = 500;
 var canvasCtx = canvas.getContext("2d");
 
 // draw an oscilloscope of the current audio source
-canvas.width = window.innerWidth;
-canvas.height = 200;
+
+
+
+
+var t = 0;
+
+
+
+var velocity = 0;
+
+// var s = document.getElementById("offset");
+// // var output = document.getElementById("demo");
+// // output.innerHTML = slider.value; // Display the default slider value
+// var dt = .01;
+// var triggerpointx = 0;
+// s.oninput = function() {
+//   velocity = parseFloat(this.value);
+//   console.log("A")
+// }
+
+
+
+
+var pastTime = audioCtx.currentTime
+var time = audioCtx.currentTime
+
+
+const arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length
+
+
+var  velocityHist = [];
+// var velocity;
+var pastX = 0;
+var triggerpointx = 0;
+vhist = [];
 function draw() {
 
   requestAnimationFrame(draw);
+  time = audioCtx.currentTime
 
   analyserNode.getByteTimeDomainData(dataArray);
 
-  canvasCtx.fillStyle = "rgb(200, 200, 200)";
+  // canvasCtx.fillStyle = "rgb(0, 61, 87)";
+  canvasCtx.fillStyle = "rgb(0, 156, 222)";
   canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
   canvasCtx.lineWidth = 1;
-  canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+  canvasCtx.strokeStyle = "rgba(255, 255, 255, 1)";
 
   canvasCtx.beginPath();
 
   var sliceWidth = canvas.width * 1.0 / bufferLength;
   var x = 0;
 
+
+  // get trigger?
+  trigger = 0
+
+
   for (var i = 0; i < bufferLength; i++) {
 
     var v = dataArray[i] / 128.0;
     var y = canvas.height / 2+(v-1)*canvas.height;
 
+    if (y>trigger) {
+      triggerpointx = x
+      trigger = y
+    }
+
+
+    x += sliceWidth;
+  }
+
+
+  if (velocityHist.length>=100) {
+    velocityHist.shift()
+
+  }
+  velocityHist.push((triggerpointx-pastX))///(time-pastTime))
+  // console.log((triggerpointx-pastX))
+  // console.log(triggerpointx-pastX)
+  // console.log("")
+  if ((triggerpointx-pastX)<=0) {
+    // console.log("WTF")
+    velocityHist.push((triggerpointx-pastX)/(time-pastTime))
+  }
+  // if ((triggerpointx-pastX)>0) {
+  //   console.log("FTW")
+  // }
+  pastX = triggerpointx
+  // velocity = arrAvg(velocityHist)
+  // console.log(velocityHist)
+  x = 0
+  for (var i = 0; i < bufferLength; i++) {
+
+    var v = dataArray[i] / 128.0;
+    var y = canvas.height / 2+(v-1)*canvas.height;
+    // console.log(velocity)
     if (i === 0) {
+      // canvasCtx.moveTo(((x-velocity*time*100)%canvas.width+canvas.width)%canvas.width, y);
+      // canvasCtx.moveTo(x-triggerpointx, y);
       canvasCtx.moveTo(x, y);
     } else {
+      // canvasCtx.lineTo(((x-velocity*time*100)%canvas.width+canvas.width)%canvas.width, y);
+      // canvasCtx.lineTo(x-triggerpointx, y);
       canvasCtx.lineTo(x, y);
     }
 
@@ -218,6 +358,8 @@ function draw() {
 
   canvasCtx.lineTo(canvas.width, canvas.height / 2);
   canvasCtx.stroke();
+
+  pastTime = audioCtx.currentTime
 }
 
 draw();
--- js/main.js
+++ js/main.js
@@ -22,3 +22,26 @@ $(document).keyup(function(event){
     $(".key:contains(" +charachter+")").css("left", "0px");
 
 });
+
+
+
+
+$(".key").bind("touchstart mousedown",function(){
+
+  charachter = this.innerHTML
+    $(".key:contains(" + charachter+")").css("box-shadow", "0px 0px 0px #222");
+    $(".key:contains(" +charachter+")").css("background", "rgba(255, 255, 255, .7)");
+    $(".key:contains(" +charachter+")").css("top", "2px");
+    $(".key:contains(" +charachter+")").css("left", "2px");
+
+});
+
+
+$(".key").bind("touchend mouseup",function(){
+
+  charachter = this.innerHTML
+  $(".key:contains(" + charachter+")").css("box-shadow", "2px 2px 5px #222");
+  $(".key:contains(" + charachter+")").css("background", "rgba(255, 255, 255, 0.5)");
+  $(".key:contains(" +charachter+")").css("top", "0px");
+  $(".key:contains(" +charachter+")").css("left", "0px");
+});
